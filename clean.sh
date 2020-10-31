8faeddecd2b08682a7b3f1a1504775fdcd796412
Kouichirou Sinistra <ksinistr@ev-q2.kzn.21-school.ru>
2020-10-31 18:47:53 +0300

    clean.sh

--- clean.sh
+++ clean.sh
@@ -3,7 +3,6 @@ brew update
 brew upgrade
 brew cleanup -s
 rm -rf ~/Library/Caches/
-rm -rf ~/Library/*42_cache*
 rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
 rm -rf ~/Library/Application\ Support/Slack/Cache/
 rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
