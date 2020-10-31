d83b39881774276180f7b3dc2922909bd7a2bee5
Artur Nasyrov <anasyrov@mail.ru>
2020-10-31 12:16:00 +0300

    Clean Slack

--- clean.sh
+++ clean.sh
@@ -2,5 +2,10 @@
 brew update
 brew upgrade
 brew cleanup -s
+rm -rf ~/Library/Caches/
+rm -rf ~/Library/*42_cache*
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
 df -h ~/
 du -hd1 ~/ | sort -h
