bb5deb2020472aae97ef630d6fb6b05791e203d7
Artur Nasyrov <anasyrov@mail.ru>
2021-05-03 14:50:05 +0300

    List all packages installed using Homebrew and their sizes

---
+++ brew-list.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+brew list --formula | xargs -n1 -P8 -I {} \
+    sh -c "brew info {} | egrep '[0-9]* files, ' | sed 's/^.*[0-9]* files, \(.*\)).*$/{} \1/'" | \
+    sort -h -r -k2 - | column -t
+
+echo 'List all package dependencies in Homebrew:'
+echo '  brew deps --tree --installed'
+echo '  brew deps --tree fontconfig'
+echo '  brew deps --include-build --tree $(brew leaves)'
+echo 'Remove a Homebrew package including all its dependencies'
+echo '  brew rm FORMULA'
+echo '  brew rm $(join <(brew leaves) <(brew deps FORMULA))'
