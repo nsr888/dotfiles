f12cc9566083b4ef023fce1b1d325a192e37c506
Artur Nasyrov <anasyrov@mail.ru>
2021-05-03 14:51:18 +0300

    Added github link

--- brew-list.sh
+++ brew-list.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://gist.github.com/eguven/23d8c9fc78856bd20f65f8bcf03e691b
 brew list --formula | xargs -n1 -P8 -I {} \
     sh -c "brew info {} | egrep '[0-9]* files, ' | sed 's/^.*[0-9]* files, \(.*\)).*$/{} \1/'" | \
     sort -h -r -k2 - | column -t
