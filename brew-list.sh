9f7ebbba17d117bfba8bac51aee944012ae1169e
Artur Nasyrov <anasyrov@mail.ru>
2021-05-03 14:52:38 +0300

    Added comments

--- brew-list.sh
+++ brew-list.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # https://gist.github.com/eguven/23d8c9fc78856bd20f65f8bcf03e691b
+echo 'List all packages installed using Homebrew and their sizes:'
 brew list --formula | xargs -n1 -P8 -I {} \
     sh -c "brew info {} | egrep '[0-9]* files, ' | sed 's/^.*[0-9]* files, \(.*\)).*$/{} \1/'" | \
     sort -h -r -k2 - | column -t
